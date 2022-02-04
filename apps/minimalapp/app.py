823527402235d7ae7374f09c4567de7b70178232
taktiks2 <deathproof.lee@gmail.com>
2022-02-04 10:38:45 +0900

    added static and templates dir

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,15 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, url_for,\
+    request, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'hello flask'
+    return "hello flask"
 
 
-@app.route('/hello/<name>',
-           methods=['GET', 'POST'],
-           endpoint='hello-endpoint')
+@app.route("/hello/<name>",
+           methods=["GET", "POST"],
+           endpoint="hello-endpoint")
 def hello(name):
-    return f'hello {name}!'
+    return f"hello {name}!"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html",
+                           the_name=name)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/contact/complete",
+           methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        return redirect(url_for("contact_complete"))
+
+    return render_template("contact_complete.html")
