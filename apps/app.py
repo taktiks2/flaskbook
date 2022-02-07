3f653f05d7d30ed8d6767c3a701f866bd3cfd4b0
taktiks2 <deathproof.lee@gmail.com>
2022-02-08 08:23:01 +0900

    added SQLAlchemy and Migrate to apps/app.py

--- apps/app.py
+++ apps/app.py
@@ -1,9 +1,29 @@
+from pathlib import Path
+from re import S
 from flask import Flask
-# appsを抜いてただのcrudにすると動かない
+import sqlalchemy
+# appsを抜いてただのcrudにすると動かない(flask run時にimport error)
 from apps.crud import views as crud_views
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    # アプリのコンフィグ設定
+    app.config.from_mapping(
+        SECRET_KEY="slkFgWiosfweoi35dkdfoDD",
+        SQLALCHEMY_DATABASE_URI=
+            f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    )
+
+    # SQLAlchemyとアプリの連携
+    db.init_app(app)
+    # Migrateとアプリの連携
+    Migrate(app, db)
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
 
