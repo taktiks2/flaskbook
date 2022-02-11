bec4d4248cc4072c4cecbfa6186ce79a66adb87c
taktiks2 <deathproof.lee@gmail.com>
2022-02-11 14:41:38 +0900

    Add forms.py to apps/crud/

---
+++ apps/crud/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+form wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, length
+
+
+class UserForm(FlaskForm):
+    # ユーザーフォームusername属性のラベルとバリデータを設定
+    username = StringField(
+        "ユーザー名",
+        validators=[
+            DataRequired(message="ユーザー名は必須です。"),
+            length(max=30, message="30文字以内で入力してください。"),
+        ],
+    )
+    # ユーザーフォームemail属性のラベルとバリデータを設定
+    email = StringField(
+        "メールアドレス",
+        validators=[
+            DataRequired(message="メールアドレスは必須です。"),
+            Email(message="メールアドレスの形式で入力してください。"),
+
+        ],
+    )
+    # ユーザーフォームpassword属性のラベルとバリデータを設定
+    passowrd = StringField(
+        "パスワード",
+        validators=[DataRequired(message="パスワードは必須です。")]
+    )
+
+    # ユーザーフォームsubmitの文言を設定
+    submit = SubmitField("新規作成")
