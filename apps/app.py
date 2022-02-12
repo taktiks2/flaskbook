c1f42a2adaf6f110c00bb736c39c902fe3dda59c
taktiks2 <deathproof.lee@gmail.com>
2022-02-12 16:05:19 +0900

    Add auth.index endpoint to views.py

--- apps/app.py
+++ apps/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from apps.crud import views as crud_views
+from apps.auth import views as auth_views
 from flask_migrate import Migrate
 from apps.crud.models import db, User
 from flask_wtf.csrf import CSRFProtect
@@ -20,5 +21,6 @@ def create_app(config_key):
     csrf.init_app(app)
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
 
     return app
